b744e648fe9bca6f1d04ab681730ae90f85d4692
Facundo-cali <facundocali1234@gmail.com>
2022-07-06 21:49:00 -0300

    ejercicio_5_js_completo

---
+++ ejercicio_5_js.js
@@ -0,0 +1,13 @@
+
+let altCm = 180;
+
+let altM = 1.80;
+
+let pesoKg = 70.67;
+
+let altRedArriba = Math.ceil(altM);
+
+let pesoRedArriba = Math.floor(pesoKg);
+
+let maxIgual = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+
